1bff4be9a53823366afabed0af8ae0d7696245ed
sdamico <sam@sdami.co>
2013-07-02 01:19:38 -0700

    .

--- graphics.js
+++ graphics.js
@@ -1,14 +1,9 @@
-var data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-data = data.concat(data, data)
-
 var textureBuffer;
 var pMatrix;
 var mvMatrix;
 var gl;
 var shaderProgram;
 var testTexture;
-var graphTimeLength = 60000;
-var graphLastTime = (new Date).getTime();
 
 function setMatrixUniforms()
 {
